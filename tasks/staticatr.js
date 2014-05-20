a1340bf2c4bdad4b4808e07c7cad9ac2d484d762
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-05-20 08:11:30 +0200

    write parsed content

--- tasks/staticatr.js
+++ tasks/staticatr.js
@@ -5,67 +5,55 @@ var _       = require('lodash');
 module.exports = function staticatrTask(grunt) {
 	'use strict';
 
-    var d = require('util').inspect;
-    var langPattern = /-([^-]*)+\.(.*)$/;
+    var d = _.partialRight(require('util').inspect, {
+        showHidden : true,
+        depth : 10,
+        colors : true
+    });
+    var langPattern = /-([^-]{2,3})+\.(.*)$/;
 
     var staticatr = function staticatr(){
         var siteModel = {};
+        
         var options = this.options({
             defaultLang : 'en'
         });
         
+        //build siteModel
         this.files.forEach(function(file){
 
-            grunt.log.debug(d(file, true));
-            
             var matches = file.src[0].match(langPattern);
             var lang    = matches ? matches[1] : options.defaultLang;
-            var url     = file.dest;
             var content = marked(grunt.file.read(file.src));
             var meta    = content.meta || {};
-            var title   = meta.title ? meta.title : path.basename(file.dest);
             var layout  = meta.layout || 'page';
-            
+            var title   = path.basename(file.dest)
+                              .replace(/\.([^.]*)$/, '')
+                              .replace('-' + lang, '');
+
             if(!siteModel[layout]){
                siteModel[layout] = {};
             }
             if(!siteModel[layout][title]){
                siteModel[layout][title] = {};
             }
-            _.merge(siteModel[layout][title], meta); 
 
+            siteModel[layout][title][lang] = _.merge({
+                content : content.html,
+                dest    : file.dest 
+            }, meta);
 
-            
-            //grunt.log.debug(file.src);
-            //console.log(lang);
-            //console.log(result.meta);
-            //console.log(result.html);
-
-            //grunt.log.debug('-------------');
+            grunt.file.write(file.dest, content.html);
         });
 
+        //build posts
+        //_.forEach(siteModel.post, function(page, title){
+            //grunt.log.debug(title);
+            //grunt.log.debug(d(page, true));
+            //grunt.log.debug("-------------");
+        //});
         
-        grunt.log.debug(d(siteModel));
-    };
-    
-    var blog = {
-        url : 'index.html',
-        content : {
-            'en' : 'html',
-            'fr' : 'hteumeule'
-        },
-        pages : [{
-            url : '/projects.html',
-            content : 'projects',
-        }],
-        posts : [{
-            title: 'My awesome markdown file',
-            author: 'Me',
-            date: 'Wed Sep 11 2013 02:00:00 GMT+0200 (CEST)',
-            publish: true,
-            tags: [ 'tag1', 'tag2' ],
-            content: ''
-        }]
+        //grunt.log.debug(d(siteModel, true));
     };
 
     var rebuildHome = function rebuildHome(){
