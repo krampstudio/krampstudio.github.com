98dc5aefe2c1834613a9b686f1a9f7997699ea15
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-09-15 22:46:07 +0200

    nav : delegate click from li to xload

--- src/js/components/x-nav.js
+++ src/js/components/x-nav.js
@@ -8,10 +8,19 @@
     xtag.register('x-nav', {
         events : {
             'tap:delegate(li)' : function(e){
+
+                //activatate an item
                 xtag.query(e.currentTarget, 'li').forEach(function(elt){
                     elt.classList.remove('active');
                 });
                 this.classList.add('active');
+    
+                //delegate the click to the x-load tag if not targeted
+                if(e.target.nodeName.toLowerCase() !== 'x-load'){
+                    xtag.query(e.target, 'x-load').forEach(function(elt){
+                        elt.load();
+                    });
+                }
             }
         },
     });
